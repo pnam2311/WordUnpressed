914b4af41d44c794eb901dcba0a9c0de69d09ddc
Nguyễn Phương Nam <pnam2311@gmail.com>
2019-12-13 22:17:40 +0700

    set up root for Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import './bootstrap';
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import App from '@/js/App';
+ 
+Vue.use(Vuetify);
+
+new Vue({
+ el: '#app',
+ render: h => h(App)
+});
+
+export default App;
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,10 @@
-<div class="container">
-</div>
+<html>
+    <head>WordUnpressed</head>
+    <body>
+        <div class="container" id="app">
+            @yield('content')
+        </div>
+        
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
